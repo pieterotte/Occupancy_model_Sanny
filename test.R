199abedb5a95931a0959c7f8a291a8c6a809f0ac
Sanny Brenninkmeijer <s.brenninkmeijer@student.rug.nl>
2024-09-02 17:57:24 +0200

    all from today

--- test.R
+++ test.R
@@ -1,3 +1,7 @@
 library(unmarked)
 data <- 1
 test
+
+## set the github connection to my account 
+system('git config -- user.email "s.brenninkmeijer@student.rug.nl"')
+system('git config -- user.name "Sanny Brenninkmeijer"')
